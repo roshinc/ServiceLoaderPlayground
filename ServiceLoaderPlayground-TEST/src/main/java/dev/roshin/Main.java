b5fa31b7eec1195bcb9d163bf1bd6df625772e83
Roshin C <idroshin2010@gmail.com>
2024-04-01 14:20:57 -0400

    Testing project

---
+++ ServiceLoaderPlayground-TEST/src/main/java/dev/roshin/Main.java
@@ -0,0 +1,20 @@
+package dev.roshin;
+
+import java.util.ServiceLoader;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        ServiceLoader<MyService> services = ServiceLoader.load(MyService.class);
+        for (MyService service : services) {
+            service.execute();
+        }
+
+        ServiceLoader<MyService> services2 = ServiceLoader.load(MyService.class);
+        for (MyService service : services2) {
+            service.execute();
+        }
+
+
+    }
+}
